46c115da23679605f42ba017ddafb4b3285a5264
Ayman Benissa <aiman.benissa.ab@gmail.com>
2021-02-01 10:37:37 -0800

    react checkpoint

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+       <h1>متجر نقطة</h1>
+       </div>
+       <div className="jumbotron">
+         <h2>نبدة عن متجرنا</h2>
+        <p>هو متجر مختص بتقديم منتجات خاصة لمستلزمات المطاعم والمقاهي والفنادق 
+          اي اننا نبيع منتجاتنا لشركات وليس للفرد, حيث نتعامل مباشراً مع مطبخكم.
+           </p>
+
+       </div>
+     </div>
+  )
+    } 
+
+export default App;
